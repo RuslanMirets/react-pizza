100696db3794a7440bdf3ed592b34ea1b4e1dc66
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-08-18 11:18:04 +0300

    Урок №4

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Categories = ({ items }) => {
+const Categories = ({ items, onClickItem }) => {
 
   const [activeItem, setActiveItem] = React.useState(null);
 
   const onSelectItem = (index) => {
     setActiveItem(index);
-  }
+  };
 
   return (
     <div className="categories">
