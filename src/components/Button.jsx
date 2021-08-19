519c63c00cf46049ec299bc8d8876c4e47ac566f
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-08-19 12:04:18 +0300

    Урок №5

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import classNames from "classnames";
 
 const Button = ({onClick, className, outline, children}) => {
@@ -11,5 +12,9 @@ const Button = ({onClick, className, outline, children}) => {
     </button>
   );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+};
 
 export default Button;
